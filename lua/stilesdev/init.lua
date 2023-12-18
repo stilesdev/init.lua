9a91ef9709e170d8be62f9d066cfa2d868ce5227
Matthew Stiles <git@stiles.dev>
2023-12-17 22:29:06 -0500

    feat: configure basic vim settings

--- lua/stilesdev/init.lua
+++ lua/stilesdev/init.lua
@@ -1,2 +1,3 @@
+require(config_namespace .. '.set')
 require(config_namespace .. '.remap')
 require(config_namespace .. '.lazy')
---
+++ lua/stilesdev/set.lua
@@ -0,0 +1,15 @@
+-- relative line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- indent with 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+-- disable line wrapping
+vim.opt.wrap = false
+
