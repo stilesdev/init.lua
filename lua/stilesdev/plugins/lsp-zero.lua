3865c03f8e955b50ce01fe48ecdc453d14106e54
Matthew Stiles <git@stiles.dev>
2023-12-17 22:32:13 -0500

    style: reindent all neovim config files

--- lua/stilesdev/plugins/lsp-zero.lua
+++ lua/stilesdev/plugins/lsp-zero.lua
@@ -1,58 +1,58 @@
 return {
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		config = function()
-			local lsp_zero = require('lsp-zero')
-			lsp_zero.extend_lspconfig()
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
 
-			lsp_zero.on_attach(function(client, bufnr)
-				-- see :help lsp-zero-keybindings
-				-- to learn the available actions
-				lsp_zero.default_keymaps({buffer = bufnr})
-			end)
+            lsp_zero.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
 
-			-- see :help lsp-zero-guide:integrate-with-mason-nvim
-			-- to learn how to use mason.nvim with lsp-zero
-			require('mason').setup({})
-			require('mason-lspconfig').setup({
-				ensure_installed = {
-					'jsonls',	-- JSON
-					'lua_ls', 	-- LUA
-					'taplo', 	-- TOML
-					'tsserver', 	-- JS/TS
-					'volar', 	-- Vue
-				},
-				handlers = {
-					lsp_zero.default_setup,
-					lua_ls = function()
-						local lua_opts = lsp_zero.nvim_lua_ls()
-						require('lspconfig').lua_ls.setup(lua_opts)
-					end
-				}
-			})
+            -- see :help lsp-zero-guide:integrate-with-mason-nvim
+            -- to learn how to use mason.nvim with lsp-zero
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    'jsonls',	-- JSON
+                    'lua_ls', 	-- LUA
+                    'taplo', 	-- TOML
+                    'tsserver', 	-- JS/TS
+                    'volar', 	-- Vue
+                },
+                handlers = {
+                    lsp_zero.default_setup,
+                    lua_ls = function()
+                        local lua_opts = lsp_zero.nvim_lua_ls()
+                        require('lspconfig').lua_ls.setup(lua_opts)
+                    end
+                }
+            })
 
-			local cmp = require('cmp')
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            local cmp = require('cmp')
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-			cmp.setup({
-				sources = {
-					{ name = 'path' },
-					{ name = 'nvim_lsp' },
-					{ name = 'nvim_lua' },
-				},
-				formatting = lsp_zero.cmp_format(),
-				mapping = cmp.mapping.preset.insert({
-					['<CR>'] = cmp.mapping.confirm({ select = true }),
-					['<C-Space>'] = cmp.mapping.complete(),
-				}),
-			})
-		end,
-	},
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+            cmp.setup({
+                sources = {
+                    { name = 'path' },
+                    { name = 'nvim_lsp' },
+                    { name = 'nvim_lua' },
+                },
+                formatting = lsp_zero.cmp_format(),
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                }),
+            })
+        end,
+    },
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 }
--- lua/stilesdev/plugins/telescope.lua
+++ lua/stilesdev/plugins/telescope.lua
@@ -1,10 +1,10 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	keys = {
-		{'<leader>pf', "<cmd>lua require('telescope.builtin').find_files()<cr>", desc = 'Find Project Files'},
-		{'<leader>gf', "<cmd>lua require('telescope.builtin').git_files()<cr>", desc = 'Find Git Files'},
-		{'<leader>ps', "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })<cr>", desc = 'Search Project Files'},
-	},
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        {'<leader>pf', "<cmd>lua require('telescope.builtin').find_files()<cr>", desc = 'Find Project Files'},
+        {'<leader>gf', "<cmd>lua require('telescope.builtin').git_files()<cr>", desc = 'Find Git Files'},
+        {'<leader>ps', "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })<cr>", desc = 'Search Project Files'},
+    },
 }
