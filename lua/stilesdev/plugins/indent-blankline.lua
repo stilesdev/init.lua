c278382b00f265835e05495eab9a718bad378da5
Matthew Stiles <mstiles92@gmail.com>
2023-12-28 20:30:12 -0500

    feat: add indent-blankline nvim plugin

---
+++ lua/stilesdev/plugins/indent-blankline.lua
@@ -0,0 +1,25 @@
+return {
+    'lukas-reineke/indent-blankline.nvim',
+    dependencies = {
+        'catppuccin',
+    },
+    config = function ()
+        local hooks = require('ibl.hooks')
+        local colors = require('catppuccin.palettes').get_palette('mocha')
+
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function ()
+            vim.api.nvim_set_hl(0, 'CurrentScope', { fg = colors.surface2 })
+        end)
+
+        require('ibl').setup({
+            indent = {
+                char = '▏',
+            },
+            scope = {
+                show_start = false,
+                show_end = false,
+                highlight = 'CurrentScope',
+            },
+        })
+    end
+}
