bcebbf80ac30ae0a916d74681563e1b67305d3fa
Matthew Stiles <mstiles92@gmail.com>
2023-12-31 00:06:55 -0500

    feat: add fugitive keymaps for commit/push/pull

--- lua/stilesdev/plugins/vim-fugitive.lua
+++ lua/stilesdev/plugins/vim-fugitive.lua
@@ -3,5 +3,8 @@ return {
     cmd = 'Git',
     keys = {
         {'<leader>gs', "<cmd>Git<cr>", desc = "Git Status"},
+        {'<leader>gc', "<cmd>Git commit<cr>", desc = "Git Commit"},
+        {'<leader>gp', "<cmd>Git push<cr>", desc = "Git Push"},
+        {'<leader>gu', "<cmd>Git pull<cr>", desc = "Git Pull"}, -- 'u' = update
     },
 }
