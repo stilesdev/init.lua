a3a83e76f51fa74848d07b4bee4a977836bc0d50
Matthew Stiles <mstiles92@gmail.com>
2024-02-02 01:46:51 -0500

    feat: add more nvim telescope keymappings

--- lua/stilesdev/plugins/telescope.lua
+++ lua/stilesdev/plugins/telescope.lua
@@ -3,9 +3,12 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
-        {'<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", desc = 'Find Files'},
-        {'<leader>gf', "<cmd>lua require('telescope.builtin').git_files()<cr>", desc = 'Git Files'},
-        {'<leader>fs', "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })<cr>", desc = 'File Search'},
+        {'<leader>ff', function() require('telescope.builtin').find_files({ find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' } }) end, desc = 'Find Files'},
+        {'<leader>gf', function() require('telescope.builtin').git_files() end, desc = 'Git Files'},
+        {'<leader>fs', function() require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') }) end, desc = 'File Search'},
+        {'<leader>fa', function() require('telescope.builtin').live_grep() end, desc = 'Live Grep'},
+        {'<leader>fw', function() require('telescope.builtin').grep_string({ search = vim.fn.expand('<cword>') }) end, desc = 'File Search (Current Word)'},
+        {'<leader>fW', function() require('telescope.builtin').grep_string({ search = vim.fn.expand('<cWORD>') }) end, desc = 'File Search (Current WORD)'}
     },
     opts = function()
         local actions = require('telescope.actions')
