c6b00e396efe556f75e7f2b94122da847cf57775
Matthew Stiles <mstiles92@gmail.com>
2024-02-16 12:11:39 -0500

    fix: show relative path to file in lualine

--- lua/stilesdev/plugins/lualine.lua
+++ lua/stilesdev/plugins/lualine.lua
@@ -10,7 +10,12 @@ return {
         sections = {
             lualine_a = {'mode'},
             lualine_b = {'branch', 'diff', 'diagnostics'},
-            lualine_c = {'filename'},
+            lualine_c = {
+                {
+                    'filename',
+                    newfile_status = true,
+                    path = 1, -- show relative path instead of just filename
+                }},
             lualine_x = {'filesize', 'encoding', 'fileformat', 'filetype'},
             lualine_y = {'progress'},
             lualine_z = {'location'}
