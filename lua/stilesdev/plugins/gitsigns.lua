4f6e1f12ad08b16699704097376128d9245c5329
Matthew Stiles <mstiles92@gmail.com>
2024-02-16 10:30:59 -0500

    feat: show inline git blame in nvim

--- lua/stilesdev/plugins/gitsigns.lua
+++ lua/stilesdev/plugins/gitsigns.lua
@@ -2,6 +2,14 @@ return {
     'lewis6991/gitsigns.nvim',
     event = {'BufReadPre', 'BufNewFile'},
     opts = {
+        current_line_blame = true,
+        current_line_blame_opts = {
+            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+            delay = 0,
+            virt_text_priority = 5000,
+        },
+        current_line_blame_formatter = '    <author> • <author_time:%Y-%m-%d> • <summary>',
+
         on_attach = function()
             local gs = package.loaded.gitsigns
 
