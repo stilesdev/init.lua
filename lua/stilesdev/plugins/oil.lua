77a27ca1b25d7d8d5d08a64d73049d792f8c803d
Matthew Stiles <mstiles92@gmail.com>
2024-12-10 21:16:28 -0500

    fix: disable oil keybinds interfering with harpoon

--- lua/stilesdev/plugins/oil.lua
+++ lua/stilesdev/plugins/oil.lua
@@ -6,6 +6,12 @@ return {
         view_options = {
             show_hidden = true,
         },
+        keymaps = {
+            -- disable keys interfering with harpoon in oil buffers
+            ['<C-j>'] = false,
+            ['<C-k>'] = false,
+            ['<C-l>'] = false,
+        },
     },
     keys = {
         { '<leader>fv', '<cmd>Oil<cr>', desc = "Open File Browser" },
