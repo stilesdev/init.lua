58c739e59d616a4c9dbfcbf28bf7e4cd49b8b748
Matthew Stiles <mstiles92@gmail.com>
2024-12-11 00:16:44 -0500

    feat: telescope help/git branch pickers

--- lua/stilesdev/plugins/telescope.lua
+++ lua/stilesdev/plugins/telescope.lua
@@ -3,8 +3,10 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
+        {'<leader>fh', function() require('telescope.builtin').help_tags() end, desc = 'Find help'},
         {'<leader>ff', function() require('telescope.builtin').find_files({ find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' } }) end, desc = 'Find Files'},
         {'<leader>gf', function() require('telescope.builtin').git_files() end, desc = 'Git Files'},
+        {'<leader>gb', function() require('telescope.builtin').git_branches() end, desc = 'Git Branches'},
         {'<leader>fs', function() require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') }) end, desc = 'File Search'},
         {'<leader>fa', function() require('telescope.builtin').live_grep() end, desc = 'Live Grep'},
         {'<leader>fw', function() require('telescope.builtin').grep_string({ search = vim.fn.expand('<cword>') }) end, desc = 'File Search (Current Word)'},
@@ -20,6 +22,10 @@ return {
                     i = {
                         ['<esc>'] = actions.close
                     },
+                },
+                file_ignore_patterns = {
+                    '/node_modules/',
+                    '/vendor/',
                 },
             },
         }
