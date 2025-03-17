3a155701e8a285e1e628c38879bca211808a2483
Matthew Stiles <mstiles92@gmail.com>
2025-03-17 15:48:21 -0400

    feat: add nvim-ts-context-commentstring for ability to comment Vue files

--- lua/stilesdev/plugins/vim-commentary.lua
+++ lua/stilesdev/plugins/vim-commentary.lua
@@ -2,4 +2,7 @@ return {
     'tpope/vim-commentary',
     event = {'BufReadPre', 'BufNewFile'},
     opt = {},
+    dependencies = {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+    },
 }
