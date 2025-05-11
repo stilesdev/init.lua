5b66e3f5065e558763b9a8952e5fe1f5d3a677b9
Matthew Stiles <mstiles92@gmail.com>
2025-05-11 01:03:22 -0400

    feat: initial migration to vim.lsp.config() and removal of lsp-zero

--- lua/stilesdev/plugins/cmp.lua
+++ lua/stilesdev/plugins/cmp.lua
@@ -14,22 +14,11 @@ return {
         {'saadparwaiz1/cmp_luasnip'},
         {'hrsh7th/cmp-nvim-lsp'},
         {'hrsh7th/cmp-nvim-lua'},
-        {
-            -- properly configure luals for editing neovim configuration
-            'folke/lazydev.nvim',
-            ft = 'lua',
-            opts = {},
-        },
     },
     config = function()
-        -- Configure autocompletion settings
-        local lsp = require('lsp-zero')
-        lsp.extend_cmp()
-
-        -- And you can configure cmp even more if you want to
         local cmp = require('cmp')
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        local cmp_action = lsp.cmp_action()
+        -- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        -- local cmp_action = lsp.cmp_action()
 
         cmp.setup({
             window = {
@@ -47,10 +36,35 @@ return {
                 completeopt = 'menu,menuone,noinsert',
             },
             preselect = cmp.PreselectMode.None,
-            formatting = lsp.cmp_format(),
+            formatting = {
+                fields = {'abbr', 'menu', 'kind'},
+                format = function(entry, item)
+                    local n = entry.source.name
+                    local label = ''
+
+                    if n == 'nvim_lsp' then
+                        label = '[LSP]'
+                    elseif n == 'nvim_lua'  then
+                        label = '[nvim]'
+                    else
+                        label = string.format('[%s]', n)
+                    end
+
+                    if item.menu ~= nil then
+                        item.menu =  string.format('%s %s', label, item.menu)
+                    else
+                        item.menu = label
+                    end
+
+                    return item
+                end,
+            },
             mapping = cmp.mapping.preset.insert({
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<CR>'] = cmp.mapping.confirm({select = false}), -- select = whether to select the first option if no option is selected during confirm
+                ['<C-y>'] = cmp.mapping.confirm({select = false}),
                 ['<C-Space>'] = cmp.mapping.complete(),
+                ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+                ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
                 ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-d>'] = cmp.mapping.scroll_docs(4),
             }),
--- lua/stilesdev/plugins/lspconfig.lua
+++ lua/stilesdev/plugins/lspconfig.lua
@@ -1,29 +1,32 @@
 return {
     'neovim/nvim-lspconfig',
-    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-    event = {'BufReadPre', 'BufNewFile'},
+    lazy = false,
     dependencies = {
+        {'williamboman/mason.nvim'},
         {'williamboman/mason-lspconfig.nvim'},
+        {
+            -- properly configure luals for editing neovim configuration
+            'folke/lazydev.nvim',
+            ft = 'lua',
+            opts = {},
+        },
+        {'hrsh7th/cmp-nvim-lsp'},
     },
     config = function()
-        local lsp = require('lsp-zero')
-        lsp.extend_lspconfig()
-
-        lsp.on_attach(function(client, bufnr)
-            -- see :help lsp-zero-keybindings
-            -- to learn the available actions
-            lsp.default_keymaps({buffer = bufnr, exclude = { 'K' }})
-
-            -- nvim 0.11.0 disables borders on lsp hover windows, this re-enables them:
-            vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover({border = \'rounded\'})<cr>', {buffer = bufnr, desc = 'Hover documentation'})
-        end)
-
-        --lsp.set_sign_icons({
-        --    error = '󱎘', --   󱎘 󰚌 󰐼
-        --    warn = '',  --    
-        --    hint = '',  --    󱕅
-        --    info = '󰙎',  --   󰙎 󰑊
-        --})
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(event)
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover({border = \'rounded\'})<cr>', {buffer = event.buf, desc = 'Hover documentation'})
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', {buffer = event.buf, desc = 'Go to definition'})
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', {buffer = event.buf, desc = 'Go to declaration'})
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', {buffer = event.buf, desc = 'Go to implementation'})
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', {buffer = event.buf, desc = 'Go to type definition'})
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', {buffer = event.buf, desc = 'Go to reference'})
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', {buffer = event.buf, desc = 'Rename symbol'})
+                vim.keymap.set('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', {buffer = event.buf, desc = 'Format file'})
+                vim.keymap.set('x', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', {buffer = event.buf, desc = 'Format selection'})
+                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', {buffer = event.buf, desc = 'Execute code action'})
+            end,
+        })
 
         vim.diagnostic.config({
             underline = true,
@@ -37,6 +40,167 @@ return {
         -- load settings for work (stored in separate private repo)
         local has_work_lsp_settings, work_lsp_settings = pcall(require, "work-lsp")
 
+        vim.lsp.config('*', {
+            capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        })
+
+        vim.lsp.config('lua_ls', {
+            settings = {
+                Lua = {
+                    -- Disable telemetry
+                    telemetry = {enable = false},
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT',
+                    },
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = {'vim'}
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            -- Make the server aware of Neovim runtime files
+                            vim.fn.expand('$VIMRUNTIME/lua'),
+                            vim.fn.stdpath('config') .. '/lua'
+                        },
+                    },
+                },
+            },
+        })
+
+        vim.lsp.config('eslint', {
+            on_attach = function(client, bufnr)
+                -- configure eslint to format on save
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    buffer = bufnr,
+                    command = 'EslintFixAll',
+                })
+
+                -- allow eslint to respond to lsp formatting requests
+                client.server_capabilities.documentFormattingProvider = true
+            end,
+        })
+
+        vim.lsp.config('lemminx', {
+            settings = {
+                xml = {
+                    server = {
+                        workDir = '~/.cache/lemminx',
+                    },
+                },
+            },
+            on_attach = function(client, bufnr)
+                -- allow lemminx to respond to lsp formatting requests
+                client.server_capabilities.documentFormattingProvider = true
+                client.server_capabilities.documentFormattingRangeProvider = true
+            end,
+        })
+
+        vim.lsp.config('intelephense', {
+            init_options = {
+                globalStoragePath = vim.fn.expand('$HOME/.local/share/nvim/intelephense'),
+                licenceKey = vim.fn.expand('$HOME/.local/share/nvim/intelephense.key'), -- key as string, or absolute path to text file containing the key
+            },
+            on_init = function(client)
+                if has_work_lsp_settings then
+                    local path = client.workspace_folders[1].name
+
+                    local project_match_found, project_env = work_lsp_settings.get_intelephense_env(path)
+
+                    if project_match_found then
+                        client.config.settings.intelephense.environment = project_env
+                        client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+                    end
+                end
+
+                client.server_capabilities.completionProvider.triggerCharacters = {
+                    -- php
+                    -- '$', '>', ':', '\\', '/', '\'', '"',
+                    '$', ':', '\\', '/', '\'', '"',
+                    --phpdoc
+                    '*',
+                    -- html/js
+                    '.', '<'
+                }
+
+                return true
+            end,
+            settings = {
+                intelephense = {
+                    format = {
+                        braces = 'k&r',
+                    },
+                },
+            },
+        })
+
+        vim.lsp.config('stylelint_lsp', {
+            -- only lint appropriate files
+            filetypes = {
+                'css',
+                'less',
+                'scss',
+                'vue',
+            },
+            settings = {
+                stylelintplus = {
+                    -- format on save
+                    autoFixOnSave = true,
+                    -- respond to lsp formatting requests
+                    autoFixOnFormat = true,
+                }
+            }
+        })
+
+        vim.lsp.config('tailwindcss', {
+            filetypes = {
+                'css',
+                'less',
+                'scss',
+                'vue',
+            },
+        })
+
+        local ts_ls_filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'json' }
+        if use_volar_hybrid_mode then
+            table.insert(ts_ls_filetypes, 'vue')
+        end
+        vim.lsp.config('ts_ls', {
+            init_options = {
+                plugins = {
+                    {
+                        name = '@vue/typescript-plugin',
+                        location = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server"),
+                        languages = { 'vue' },
+                    },
+                },
+            },
+            filetypes = ts_ls_filetypes,
+        })
+
+        vim.lsp.config('volar', {
+            -- restrict Volar to only Vue/Nuxt projects
+            root_markers = {
+                'vue.config.js',
+                'vue.config.ts',
+                'nuxt.config.js',
+                'nuxt.config.ts',
+            },
+            init_options = {
+                vue = {
+                    hybridMode = use_volar_hybrid_mode,
+                },
+            },
+            -- disable Volar LSP formatting
+            on_init = function(client)
+                client.server_capabilities.documentFormattingProvider = false
+                client.server_capabilities.documentFormattingRangeProvider = false
+            end,
+        })
+
+        require('mason').setup()
         require('mason-lspconfig').setup({
             ensure_installed = {
                 'eslint',        -- JS/TS Linting
@@ -50,155 +214,6 @@ return {
                 'taplo',         -- TOML
                 'ts_ls',         -- JS/TS
                 'volar',         -- Vue
-            },
-            handlers = {
-                lsp.default_setup,
-                lua_ls = function()
-                    -- Optional: configure lua language server for neovim
-                    local lua_opts = lsp.nvim_lua_ls()
-                    require('lspconfig').lua_ls.setup(lua_opts)
-                end,
-                eslint = function()
-                    require('lspconfig').eslint.setup({
-                        on_attach = function(client, bufnr)
-                            -- configure eslint to format on save
-                            vim.api.nvim_create_autocmd("BufWritePre", {
-                                buffer = bufnr,
-                                command = 'EslintFixAll',
-                            })
-
-                            -- allow eslint to respond to lsp formatting requests
-                            client.server_capabilities.documentFormattingProvider = true
-                        end,
-                    })
-                end,
-                lemminx = function ()
-                    require('lspconfig').lemminx.setup({
-                        settings = {
-                            xml = {
-                                server = {
-                                    workDir = '~/.cache/lemminx',
-                                },
-                            },
-                        },
-                        on_attach = function(client, bufnr)
-                            -- allow lemminx to respond to lsp formatting requests
-                            client.server_capabilities.documentFormattingProvider = true
-                            client.server_capabilities.documentFormattingRangeProvider = true
-                        end,
-                    })
-                end,
-                intelephense = function ()
-                    require('lspconfig').intelephense.setup({
-                        init_options = {
-                            globalStoragePath = vim.fn.expand('$HOME/.local/share/nvim/intelephense'),
-                            licenceKey = vim.fn.expand('$HOME/.local/share/nvim/intelephense.key'), -- key as string, or absolute path to text file containing the key
-                        },
-                        on_init = function(client)
-                            if has_work_lsp_settings then
-                                local path = client.workspace_folders[1].name
-
-                                local project_match_found, project_env = work_lsp_settings.get_intelephense_env(path)
-
-                                if project_match_found then
-                                    client.config.settings.intelephense.environment = project_env
-                                    client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
-                                end
-                            end
-
-                            client.server_capabilities.completionProvider.triggerCharacters = {
-                                -- php
-                                -- '$', '>', ':', '\\', '/', '\'', '"',
-                                '$', ':', '\\', '/', '\'', '"',
-                                --phpdoc
-                                '*',
-                                -- html/js
-                                '.', '<'
-                            }
-
-                            return true
-                        end,
-                        settings = {
-                            intelephense = {
-                                format = {
-                                    braces = 'k&r',
-                                },
-                            },
-                        },
-                    })
-                end,
-                stylelint_lsp = function ()
-                    require('lspconfig').stylelint_lsp.setup({
-                        -- only lint appropriate files
-                        filetypes = {
-                            'css',
-                            'less',
-                            'scss',
-                            'vue',
-                        },
-                        settings = {
-                            stylelintplus = {
-                                -- format on save
-                                autoFixOnSave = true,
-                                -- respond to lsp formatting requests
-                                autoFixOnFormat = true,
-                            }
-                        }
-                    })
-                end,
-                tailwindcss = function ()
-                    require('lspconfig').tailwindcss.setup({
-                        filetypes = {
-                            'css',
-                            'less',
-                            'scss',
-                            'vue',
-                        },
-                    })
-                end,
-                ts_ls = function()
-                    local vue_ls_path = require('mason-registry').get_package('vue-language-server'):get_install_path()
-
-                    local filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'json' }
-
-                    if use_volar_hybrid_mode then
-                        table.insert(filetypes, 'vue')
-                    end
-
-                    require('lspconfig').ts_ls.setup({
-                        init_options = {
-                            plugins = {
-                                {
-                                    name = '@vue/typescript-plugin',
-                                    location = vue_ls_path .. '/node_modules/@vue/language-server',
-                                    languages = { 'vue' },
-                                },
-                            },
-                        },
-                        filetypes = filetypes,
-                    })
-                end,
-                volar = function()
-                    require('lspconfig').volar.setup({
-                        -- restrict Volar to only Vue/Nuxt projects
-                        root_dir = require("lspconfig").util.root_pattern(
-                            "vue.config.js",
-                            "vue.config.ts",
-                            "nuxt.config.js",
-                            "nuxt.config.ts"
-                        ),
-                        init_options = {
-                            vue = {
-                                hybridMode = use_volar_hybrid_mode,
-                            },
-                        },
-                        -- disable Volar LSP formatting
-                        on_init = function(client)
-                            client.server_capabilities.documentFormattingProvider = false
-                            client.server_capabilities.documentFormattingRangeProvider = false
-                        end,
-                    })
-                end,
             },
         })
     end,
