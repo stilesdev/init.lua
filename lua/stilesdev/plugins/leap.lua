6899d6dd8166b5712398310118b255b9142696ad
Matthew Stiles <mstiles92@gmail.com>
2026-04-24 23:49:37 -0400

    fix: update leap.nvim config to handle removed default keybinds

--- lua/stilesdev/plugins/leap.lua
+++ lua/stilesdev/plugins/leap.lua
@@ -3,12 +3,9 @@ return {
     dependencies = {
         'tpope/vim-repeat',
     },
-    keys = {
-        's',
-        'S',
-        'gs',
-    },
     config = function ()
-        require('leap').create_default_mappings()
+        vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
+        vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
+        vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
     end,
 }
